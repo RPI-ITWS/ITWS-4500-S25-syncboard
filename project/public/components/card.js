8727ad6e84577ab37bfd47ed471858dd28632ba4
Morgan Myers <myersm6@rpi.edu>
2025-02-23 14:30:43 -0500

    fix card/workspace linking

--- project/public/components/card.js
+++ project/public/components/card.js
@@ -1,13 +1,15 @@
-const Card = ({ title, day, time, location, color }) => {
-    return (
-        <div className="card">
-            <div className="card-header" style={{ backgroundColor: `var(${color})` }}></div>
-            <div className="card-body">
-                <h3>{title}</h3>
-                <p>{day}</p>
-                <p>{time}</p>
-                <p>{location}</p>
+const Card = ({ title, day, time, location, color, to }) => {
+    return ( 
+        <ReactRouterDOM.Link to={to} className="card-link">
+            <div className="card"> 
+                <div className="card-header" style={{ backgroundColor: `var(${color})` }}></div>
+                <div className="card-body">
+                    <h3>{title}</h3>
+                    <p>{day}</p>
+                    <p>{time}</p>
+                    <p>{location}</p>
+                </div>
             </div>
-        </div>
+        </ReactRouterDOM.Link>
     );
 };
--- project/public/components/workspace.js
+++ project/public/components/workspace.js
@@ -1,8 +1,8 @@
 const Workspaces = () => {
     const workspaces = [
-        { title: "Web Science", day: "Tuesday/Friday", time: "8:00-9:50am", location: "Lally 102", color: "--orange" },
-        { title: "Intro to Biology", day: "Tuesday/Friday", time: "10:00-11:50am", location: "Sage 2510", color: "--yellow" },
-        { title: "Data Structures", day: "Monday/Thursday", time: "2:00-3:50pm", location: "DCC 337", color: "--blue" }
+        { title: "Web Science", day: "Tuesday/Friday", time: "8:00-9:50am", location: "Lally 102", color: "--orange", to:"/home/dashboard" },
+        { title: "Intro to Biology", day: "Tuesday/Friday", time: "10:00-11:50am", location: "Sage 2510", color: "--yellow",to:"/home/dashboard" },
+        { title: "Data Structures", day: "Monday/Thursday", time: "2:00-3:50pm", location: "DCC 337", color: "--blue", to:"/home/dashboard" }
     ];
 
     return (
